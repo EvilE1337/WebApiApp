49ce75b7e2ae889581967fced8474dc1d6be6150
Dmitry Pogonyiaev <lounge000@mail.ru>
2021-09-16 13:02:46 +0300

    Add EF

---
+++ AppDatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiApp.Models;
+
+namespace WebApiApp
+{
+    public class AppDatabaseContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
